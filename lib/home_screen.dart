e45fcc5d680d8b1e20f6730da240d7129a8aa9b9
Abhijeet Vishwakarma <vishwakarma2003@hotmail.com>
2022-10-05 16:23:26 +0530

    HomeScreen Added

---
+++ lib/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class homeScreen extends StatefulWidget {
+  const homeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<homeScreen> createState() => _homeScreenState();
+}
+
+class _homeScreenState extends State<homeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('API Intergrate')),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}

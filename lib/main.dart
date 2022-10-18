0a0d87337a5c1f19e453563354c212902a7965d6
Abhijeet Vishwakarma <vishwakarma2003@hotmail.com>
2022-10-18 23:54:48 +0530

    New Files

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:api/example_two.dart';
 import 'package:api/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:api/example_three.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const example_2(),
+      home: const exsmple_3(),
     );
   }
 }
